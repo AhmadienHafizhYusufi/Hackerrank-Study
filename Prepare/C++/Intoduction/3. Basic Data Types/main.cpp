1b0b01e0f68eb1dcc3593340966d29509dd09685
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-02-12 15:26:09 +0700

    Basic Data Types
    Basic Data Types in C++

---
+++ Prepare/C++/Intoduction/3. Basic Data Types/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char ch;
+    float f;
+    double d;
+    
+    scanf("%d %ld %c %f %lf", &i, &l, &ch, &f, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf", i, l, ch, f, d);
+
+    // cin >> i >> l >> ch >> fl >> d;
+    // cout << i << endl << l << endl << ch << endl << fl << endl << d;
+    return 0;
+}
