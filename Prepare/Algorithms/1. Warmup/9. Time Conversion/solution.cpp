2effa68282d7baec5ac3056671daebcef0ae0d44
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-02-15 23:14:33 +0700

    Algorithms
    Solving Time Conversion problem.

--- Prepare/Algorithms/1. Warmup/9. Time Conversion/solution.cpp
+++ Prepare/Algorithms/1. Warmup/9. Time Conversion/solution.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -10,6 +12,26 @@ using namespace std;
  */
 
 string timeConversion(string s) {
+    int hour = stoi(s.substr(0, 2));
+    string minute = s.substr(3, 2);
+    string second = s.substr(6, 2);
+    string period = s.substr(8, 2);
+
+    if (period == "AM") {
+        if (hour == 12) {
+            hour = 0;
+        }
+    } else {
+        if (hour != 12) {
+            hour += 12;
+        }
+    }
+
+     // Format the hour to be two digits
+     string hourStr = (hour < 10) ? "0" + to_string(hour) : to_string(hour);
+    
+     // Construct the 24-hour format time
+     return hourStr + ":" + minute + ":" + second;
 
 }
 
